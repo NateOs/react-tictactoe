1f71a5dadef32f0b04ba8aa8f930c964fb61c090
nateos <sodjanathan@gmail.com>
2021-02-02 20:50:17 +0000

    Added winner determiner fxn called fxn to Board Checked turns to determine winner

--- src/index.js
+++ src/index.js
@@ -17,13 +17,16 @@ function Square(props) {
     constructor(props) {
         super(props)
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
     handleClick(i) {
-        const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        const squares = this.state.squares.slice() //copy value of state
+        squares[i] = this.state.xIsNext ? 'X' : 'O' //enabling second player 'O'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext})
     }
     renderSquare(i) {
       return(<Square value = {this.state.squares[i]}  //passing props to square (value and onclick)
@@ -32,9 +35,15 @@ function Square(props) {
       )
     }
   
-    render() {
-      const status = 'Next player: X';
-  
+    render() { 
+        //determining winner and turns
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if (winner) {
+            status = 'Winner: ' + winner 
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0')
+        }
       return (
         <div>
           <div className="status">{status}</div>
@@ -80,4 +89,24 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
