7a3ac575aab55da94cbc9dcf9cfc82790c626722
nateos <sodjanathan@gmail.com>
2021-02-02 14:00:52 +0000

    Passed Prop from Parent Board to Child Square component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
+class Square extends React.Component { //Child Component
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
   }
   
-  class Board extends React.Component {
+  class Board extends React.Component { //Parent Component
     renderSquare(i) {
-      return <Square />;
+      return <Square value = {i} />;
     }
   
     render() {
